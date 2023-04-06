b97336646d758cde4620e3b888cfb5cd3bf46b19
vladuhalys <vlad.lysenko1999@gmail.com>
2023-04-06 19:01:08 +0300

    Update

--- application.cpp
+++ application.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
-#include "single_link_list.h"
+#include "doubly_linked_list.h"
 using namespace std;
 
 int main()
 {
-	single_linked_list<string> list;
+	doubly_linked_list<string> list;
 	list.push_back("London");
 	list.push_back("Paris");
 	list.push_back("New York");
 	list.push_back("Tokyo");
-	list.push_back("Moscow");
 	list.push_back("Berlin");
 	list.push_back("Rome");
 	list.push_back("Madrid");
-	list.dev_print();
-	list.insert(3,"Kiev");
-	list.dev_print();
+	list.push_front("Cities: ");
+	list.print();
+	
 	return 0;
 }
